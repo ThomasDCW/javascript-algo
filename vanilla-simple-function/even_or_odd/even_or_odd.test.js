dc11dd2eae3e705b393c37a06d05eccf0a2a113a
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-01-20 10:35:33 +0100

    test even or odd

--- vanilla-simple-function/even_or_odd/even_or_odd.test.js
+++ vanilla-simple-function/even_or_odd/even_or_odd.test.js
@@ -1,6 +1,9 @@
 const even_or_odd = require('./even_or_odd');
 describe('even or odd', () => {
-  it('should return even if number= 2', () => {
+  it('should return Even if number = 2', () => {
     expect(even_or_odd(2)).toBe('Even');
+  });
+  it('should return Odd if number = 3', () => {
+    expect(even_or_odd(3)).toBe('Odd');
   });
 });
